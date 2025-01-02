912c7e24b16b4615e0fa6dbf21676c667c53f625
arnoldnekemiah <arnoldnek@gmail.com>
2025-01-02 16:34:25 +0300

    updated user relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_many :services, dependent: :destroy
+    has_many :supports, dependent: :destroy
+
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true
 end
